3979d60dc11e3ea71b658c0195d8b2b3adee84f3
Victor Chiao <99703014@nccu.edu.tw>
2013-05-19 17:55:27 +0800

    first commit

---
+++ Documents/iOS/Practice/Practice/MUAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MUAppDelegate.h
+//  Practice
+//
+//  Created by Hank on 13/5/19.
+//  Copyright (c) 2013年 Hank. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MUAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Documents/iOS/Practice/Practice/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Practice
+//
+//  Created by Hank on 13/5/19.
+//  Copyright (c) 2013年 Hank. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MUAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MUAppDelegate class]));
+    }
+}
